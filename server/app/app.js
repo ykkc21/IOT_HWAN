b44af558753dde1b84c091dd9b560a4acd6bacd4
yewon-Noh <yewo2nn16@gmail.com>
2021-06-02 02:36:21 +0900

    socket server

--- server/app/app.js
+++ server/app/app.js
@@ -1,15 +1,31 @@
-const express = require("express");
+//소켓 서버는 연결을 위한 수신 처리 후 서버 연결을 통한 데이터 송신/수신을 한다.
+//net모듈의 소켓 객체를 사용
+var net = require('net');
 
-const app = express();
+//서버를 생성
+var server = net.createServer(function(client){
+    console.log('Client connected');
 
-const port = 8080;
+    //Client로 부터 오는 data를 화면에 출력
+    client.on('data', function(data){
+        console.log('Client sent ' + data.toString());
+    });
 
-app.set("port", port);
+    //Client와 접속이 끊기는 메시지 출력
+    client.on('end',function(){
+        console.log('Client disconnected');
+    });
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+    //Client가 접속하면 화면에 출력해주는 메시지
+    client.write('Hello');
 });
 
-app.listen(port, () => console.log("Listening on", port));
+//에러가 발생할 경우 화면에 에러 메시지 출력
+server.on('error',function(err){
+    console.log('err'+err);
+})
 
-module.exports = app;
+//port 8080으로 접속이 가능하도록 대기
+server.listen(8080, function(){
+    console.log('Server listening for connections');
+});
