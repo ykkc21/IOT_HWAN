b3b0284e5019de13b0c8b22277a9a6b015b42081
yewon-Noh <yewo2nn16@gmail.com>
2021-06-02 15:06:01 +0900

    test

--- server/app/app.js
+++ server/app/app.js
@@ -1,39 +1,18 @@
-const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
 const path = require('path');
-
-
-const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
-
 
 app.get('/', (req, res) => {
     // res.send('Hello, Express');
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-const server = app.listen(app.get('port'), () =>{
+app.listen(app.get('port'), () =>{
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
-// const io = socket;
-// io.on('connection', (socket) => { // 웹 소켓 연결시
-//     console.log('hello');
-
-//     socket.on('reply', (data) => {
-//         console.log(data);
-//         io.emit(data);
-//     });
-// });
-
-const socket = require('socket.io')(server);
-const io = socket;
-io.on('connection', function(socket){
-    socket.emit('message_from_server', 'hello, world');
-
-    socket.on('reply', (data) => {
-        console.log(data);
-        socket.emit('server: ' + data);
-    });
-});
