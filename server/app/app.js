3f06bda880521209a5c5da9777294d5d7ee254a4
yewon-Noh <yewo2nn16@gmail.com>
2021-06-02 13:55:09 +0900

    web socket

--- server/app/app.js
+++ server/app/app.js
@@ -2,7 +2,7 @@ var app = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.length('/', function(req, res){
+app.get('/', function(req, res){
     res.sendfile('index.html'); //default page
 });
 
