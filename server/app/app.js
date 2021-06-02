f22d652a1bfee5ca1d4ccf21e61593d92d6ab701
yewon-Noh <yewo2nn16@gmail.com>
2021-06-02 14:31:38 +0900

    Update app.js

--- server/app/app.js
+++ server/app/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
@@ -9,6 +11,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(app.get('port'), () =>{
+const server = app.listen(app.get('port'), () =>{
     console.log(app.get('port'), '번 포트에서 대기 중');
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log('hello');
 });
