7f5ea04c61e64b2692e021c25a8fd8c63320ea3b
yewon-Noh <yewo2nn16@gmail.com>
2021-06-02 14:01:00 +0900

    Update app.js

--- server/app/app.js
+++ server/app/app.js
@@ -3,7 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.sendfile('index.html'); //default page
+    // res.sendfile('index.html'); //default page
+    console.log(res);
 });
 
 http.listen(3000, function(){
