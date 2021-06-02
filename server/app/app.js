082b9f8695e9e27b9005e400ae80992bc7554750
yewon-Noh <yewo2nn16@gmail.com>
2021-06-02 14:18:25 +0900

    express test

--- server/app/app.js
+++ server/app/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express');
+    // res.send('Hello, Express');
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(app.get('port'), () =>{
