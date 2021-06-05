dc910b556ae882f1d0567655beaaac1f3de3c8e2
yewon-Noh <yewo2nn16@gmail.com>
2021-06-06 00:20:36 +0900

    Update app-mysql.js

--- server/app/app-mysql.js
+++ server/app/app-mysql.js
@@ -12,12 +12,27 @@ var connection = mysql.createConnection({
 connection.connect();
 
 // 쿼리 수행
-connection.query('SELECT name FROM roomInfo WHERE room = \'203\'', function(error, results, fields){
-    if (error){
-        console.log(error);
-    }
-    console.log(results);
-});
+// connection.query('SELECT name FROM roomInfo WHERE room = \'203\'', function(error, results, fields){
+//     if (error){
+//         console.log(error);
+//     }
+//     console.log(results);
+// });
+
+var id = 101;
+var sql = 'SELECT name FROM roomInfo WHERE room = \'' + id + '\'';
+        // var name = '';
+        connection.query(sql, function(error, rows, fields){
+            if (error){
+                console.log(error);
+            }
+            console.log(rows);
+
+            // name = rows.name;
+
+            // res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+            // res.write("Success, name: " + name);
+        });
 
 // 데이터베이스 연결해제
 connection.end();
