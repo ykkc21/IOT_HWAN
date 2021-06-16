d32f2cd36eaa9d897eaf755c7d5d9c8bbacfed43
yewon-Noh <yewo2nn16@gmail.com>
2021-06-16 15:36:32 +0900

    Update arduino.js

--- server/app/arduino.js
+++ server/app/arduino.js
@@ -5,6 +5,18 @@ const path = require('path');
 const android = require('./app')
 const mysql = require('mysql')
 
+// 데이터베이스 부분
+// 데이터베이스와 연결할 객체 생성
+var connection = mysql.createConnection({
+    // DataBase 주소
+    host : 'localhost',
+    // user
+    user : 'hwanroot',
+    // password
+    password : '1234',
+    // DataBase 이름
+    database : 'hwan'
+})
 
 // 데이터베이스 연결
 connection.connect();
